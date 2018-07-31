bb0683cf4b2f48151a46a7a782089908c460ddc6
Mujib517 <Mujib517@gmail.com>
2018-07-31 08:57:44 +0530

    More validations

--- models/book.model.js
+++ models/book.model.js
@@ -1,9 +1,17 @@
 var mongoose = require('mongoose');
 
+
+function validatePrice(price) {
+  return price > 100;
+}
+
 var model = mongoose.model("Book", {
-  name: { type: String, required: [true, "Required"] },
+  name: {
+    type: String, required: [true, "Required"],
+    minlength: [3, "Min 3 chars"], maxlength: [10, "Max 10 Chars"]
+  },
   author: { type: String, required: [true, "Required"] },
-  price: { type: Number, required: [true, "Required"] },
+  price: { type: Number, required: [true, "Required"], validate: { validator: validatePrice,message:"Invalid Price" } },
   inStock: { type: Boolean, default: false },
   lastUpdated: { type: Date, default: Date.now }
 });
